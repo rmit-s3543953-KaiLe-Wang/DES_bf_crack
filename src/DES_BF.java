f81c77cd4c7a273da48ae7daeef3d0797c6c3e7a
rmit-s3543953-KaiLe-Wang <30993442+rmit-s3543953-KaiLe-Wang@users.noreply.github.com>
2018-08-13 20:18:12 +1000

    1st version

--- src/DES_BF.java
+++ src/DES_BF.java
@@ -1,5 +1,7 @@
 import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import javax.xml.bind.DatatypeConverter;
 import java.math.BigInteger;
@@ -10,9 +12,20 @@ import javax.crypto.spec.SecretKeySpec;
 
 public class DES_BF {
 	static int[] targetInDec=new int[26];
-	
-	public static void outConsole(String decryptedMessage,String key){
-		System.out.println("Decrypted message: "+decryptedMessage+"\t"+". With key value of: 0x"+key);
+	private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
+	static int counter=0;
+	public static String bytesToHex(byte[] bytes) {
+	    char[] hexChars = new char[bytes.length * 2];
+	    for ( int j = 0; j < bytes.length; j++ ) {
+	        int v = bytes[j] & 0xFF;
+	        hexChars[j * 2] = hexArray[v >>> 4];
+	        hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+	    }
+	    return new String(hexChars);
+	}
+	public static void outConsole(String decryptedMessage,String key,PrintWriter writer){
+		writer.println("message: "+decryptedMessage+"\t"+". key value of: 0x"+key.toString());
+		//System.out.println("Decrypted message: "+decryptedMessage+"\t"+". With key value of: 0x"+key.toString());
 	}
 	public static byte[] hexStringToByteArray(String s) {
 	    int len = s.length();
@@ -41,25 +54,10 @@ public class DES_BF {
 		byte[] decrypted = cipher.doFinal(encrypted);
 		return decrypted;
 	}
-	public static byte[] makeSMBKey(byte[] key7, byte[] key8) {
-
-	    int i;
-
-	    key8[0] = (byte) ( ( key7[0] >> 1) & 0xff);
-	    key8[1] = (byte)(( ((key7[0] & 0x01) << 6) | (((key7[1] & 0xff)>>2) & 0xff)) & 0xff );
-	    key8[2] = (byte)(( ((key7[1] & 0x03) << 5) | (((key7[2] & 0xff)>>3) & 0xff)) & 0xff );
-	    key8[3] = (byte)(( ((key7[2] & 0x07) << 4) | (((key7[3] & 0xff)>>4) & 0xff)) & 0xff );
-	    key8[4] = (byte)(( ((key7[3] & 0x0F) << 3) | (((key7[4] & 0xff)>>5) & 0xff)) & 0xff );
-	    key8[5] = (byte)(( ((key7[4] & 0x1F) << 2) | (((key7[5] & 0xff)>>6) & 0xff)) & 0xff );
-	    key8[6] = (byte)(( ((key7[5] & 0x3F) << 1) | (((key7[6] & 0xff)>>7) & 0xff)) & 0xff );
-	    key8[7] = (byte)(key7[6] & 0x7F);
-	    for (i=0;i<8;i++) {
-	        key8[i] = (byte)( key8[i] << 1);
-	    }
-	    return key8;
-	}
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
 		// TODO Auto-generated method stub
+		
+		BigInteger[] dataIn=new BigInteger[10000];
 		/*1. set cyphertext and then convert to byte*/
 		String cypherTxtOrigin="ce126d2ddf2d1e64";
 		byte[] cipherTxtByte=hexStringToByteArray(cypherTxtOrigin);
@@ -72,17 +70,30 @@ public class DES_BF {
 		String post = Integer.toString(postfix);
 		System.out.println(post);
 		BigInteger keyMin=new BigInteger("0000000000");
-		//System.out.println(keyMin.toString(16));
 		BigInteger keyFix = new BigInteger(post);
+		BigInteger step = new BigInteger("1099511");
 		BigInteger keyMax=new BigInteger("1099511627776");
+		for (int i =0;i<10000-2;i++)
+		{
+			dataIn[i]=BigInteger.ZERO;
+			BigInteger mult= new BigInteger(String.valueOf(i));
+			dataIn[i]=dataIn[i].add(step.multiply(mult));
+			System.out.println(dataIn[i]);
+		}
+		dataIn[9999]=keyMax;
+		/*
 		BigInteger keyHalf = new BigInteger("549755813888");
 		BigInteger keyQuat = new BigInteger("274877906944");
+		BigInteger keyTenth= new BigInteger("109951162778");
+		*/
 		/*3. convert them to Hex string*/
-		String KeyMinHex=keyMin.toString(16);
+		//String KeyMinHex=keyMin.toString(16);
 		String KeyMaxHex=keyMax.toString(16);
 		System.out.println(KeyMaxHex+" L="+KeyMaxHex.length());
-		String KeyHalfHex=keyHalf.toString(16);
-		String KeyQuatHex=keyQuat.toString(16);
+		/*
+		 * String KeyHalfHex=keyHalf.toString(16);
+		   String KeyQuatHex=keyQuat.toString(16);
+		*/
 		String fixHex=keyFix.toString(16);
 		/*
 		 * Note for displaying bytes. Java uses signed digit.
@@ -90,36 +101,39 @@ public class DES_BF {
 		 * */
 		//System.out.println(Arrays.toString(toByteArray(KeyMaxHex)));
 		/*4. convert to byte and then implement*/
-		byte[] keyMaxByte=toByteArray(KeyQuatHex);
 		byte[] keyAttempted =toByteArray("0000000000");
 		byte fixedB[] = toByteArray(fixHex);
-		//while(keyMin.compareTo(keyQuat)!=0)
 		try {	
-			
-			for (int i=0; i <10;i++)
+			for(int i=0;i<dataIn.length;i++)
 			{
-				ByteArrayOutputStream outputStream = new ByteArrayOutputStream( );
-				keyAttempted =toByteArray(keyMin.toString());
-				if(keyAttempted.length<6)
+				String filename="Decrypted"+counter+".txt";
+				PrintWriter writer = new PrintWriter(filename, "UTF-8");
+				while(keyMin.compareTo(dataIn[i])!=0)
+					//for (int i=0; i <100;i++)
 				{
-					byte[] temp = new byte[5];
-					System.arraycopy(keyAttempted, 0, temp, 0, keyAttempted.length);
-					keyAttempted=temp;
+					long keyValInHex=keyMin.longValue();
+					keyAttempted =toByteArray(Long.toHexString(keyValInHex));
+					if(keyAttempted.length<6)
+					{
+						byte[] temp = new byte[6];
+						System.arraycopy(keyAttempted, 0, temp, 6-keyAttempted.length, keyAttempted.length);
+						keyAttempted=temp;
+					}
+					//key+postfix
+					byte[] key = new byte[keyAttempted.length + fixedB.length];
+					System.arraycopy(keyAttempted, 0, key, 0, keyAttempted.length);
+					System.arraycopy(fixedB, 0, key, keyAttempted.length, fixedB.length);
+					//System.out.println("keyat: "+key.length+"fix: "+fixedB.length+"key length: "+key.length+" key:"+bytesToHex(key));
+					//decrypt
+					byte[] dbyte = decrypt(key,cipherTxtByte);
+					//increment
+					keyMin=keyMin.add(BigInteger.ONE);
+					String decryptedMessage = new String(dbyte);
+					outConsole(decryptedMessage,bytesToHex(key), writer);
 				}
-				//key+postfix
-				byte[] key = new byte[keyAttempted.length + fixedB.length];
-				System.arraycopy(fixedB, 0, key, 0, fixedB.length);
-				System.arraycopy(keyAttempted, 0, key, fixedB.length, keyAttempted.length);
-				
-				byte[] key8 = new byte[8];
-				key=makeSMBKey(key, key8 );
-				System.out.println("keyat: "+key.length+"fix: "+fixedB.length+"key: "+key.length);
-				//decrypt
-				byte[] dbyte = decrypt(key,cipherTxtByte);
-				//increment
-				keyMin=keyMin.add(BigInteger.ONE);
-				String decryptedMessage = new String(dbyte);
-				outConsole(decryptedMessage,Arrays.toString(key));
+				System.out.println("One file compeleted! The file name is:"+filename);
+				counter++;
+				writer.close();
 			}
 		}
 		 catch (Exception e) {
